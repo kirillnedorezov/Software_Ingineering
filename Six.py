d7e6eadbc59b24f3a8c9ea4ca5fde96c0391927c
Kirill <nedorez02@gmail.com>
2023-10-12 14:39:34 +0500

    Six task

---
+++ Six.py
@@ -0,0 +1,7 @@
+numOne = 15
+numTwo = 9
+
+print('Возведение в степень – ', numOne ** numTwo)
+print('Обычное деление – ', numOne / numTwo)
+print('Целочисленное деление – ', numOne // numTwo)
+print('Нахождение остатка от деления – ', numOne % numTwo)
